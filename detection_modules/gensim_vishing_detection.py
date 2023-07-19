a98945c67ad6cf86eb62c2b8cb014f339f2cd6cc
WaafiAdam <abdullah.waafi@gmail.com>
2023-07-19 12:27:01 +0800

    fix: spacy, nltk

--- detection_modules/gensim_vishing_detection.py
+++ detection_modules/gensim_vishing_detection.py
@@ -1,5 +1,7 @@
 import pandas as pd
 from gensim import corpora, models, similarities
+import nltk
+nltk.download('stopwords')
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 import time
