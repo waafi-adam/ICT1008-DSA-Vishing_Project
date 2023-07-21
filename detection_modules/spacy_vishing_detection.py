6e943e019e34e5f1ca3a600e39035b6c5879739c
WaafiAdam <abdullah.waafi@gmail.com>
2023-07-21 08:44:54 +0800

    change en-core to small

--- detection_modules/spacy_vishing_detection.py
+++ detection_modules/spacy_vishing_detection.py
@@ -7,7 +7,7 @@ import csv
 preprocessing_start_time = time.time()
 
 # Load the spaCy English model
-nlp = spacy.load("en_core_web_md")
+nlp = spacy.load("en_core_web_sm")
 
 def preprocess_sentence(sentence):
     doc = nlp(sentence)
